bcf874882861bd53c6a23c283f856a62ca520409
fadhil riyanto <me@fadev.org>
2025-11-07 20:02:38 +0700

    add macro to show bit status in various datatype
    Signed-off-by: fadhil riyanto <me@fadev.org>

---
+++ src/vt_bitfield.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifndef VT_BITFIELD_DUMP
+#define VT_BITFIELD_DUMP(x)                                                    \
+        do {                                                                   \
+                u_int8_t mod = 16;                                              \
+                u_int8_t nstart = 1;                                           \
+                u_int8_t size = sizeof(x) * 8;                                 \
+                char status[20];                                               \
+                                                                               \
+                for (u_int8_t i = 1; i < mod + 1; i++) {                       \
+                        nstart = i;                                            \
+                        for (u_int8_t ii = 0; ii < (size / mod); ii++) {       \
+                                u_int8_t temp = (x >> nstart - 1) & 0x1;       \
+                                if (temp == 1)                                 \
+                                        strcpy(status, "\x1b[33mtrue\x1b[0m"); \
+                                else                                           \
+                                        strcpy(status, "false");               \
+                                                                               \
+                                (nstart < 9)                                   \
+                                    ? printf("%d = %s\t ", nstart, status)     \
+                                    : printf("%d = %s\t", nstart, status);     \
+                                nstart = nstart + mod;                         \
+                        }                                                      \
+                        printf("\n");                                          \
+                }                                                              \
+        } while (0);
+
+#endif
